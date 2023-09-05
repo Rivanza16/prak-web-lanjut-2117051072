415db5843b1d401120d4fd08ffc00878c7369d51
Rivanza16 <rivanza.syifa21@students.unila.ac.id>
2023-09-05 12:29:15 +0700

    mengganti kalimat

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,9 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
-
-        <h2>The small framework with powerful features</h2>
+        <h1>Rivanza Syifa Pujamara - 2117051072</h1>
 
     </div>
 
