7c5a5ea5f6ac191c1aed1f682d2e03ad8adc741e
Rivanza16 <rivanza.syifa21@students.unila.ac.id>
2023-10-01 22:01:43 +0700

    update list_user

---
+++ app/Views/layouts/app.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title?></title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('./assets/css/style.css'); ?>">  
+</head>
+
+<body style="background-image: url(<?= base_url('./assets/img/backg.jpg') ?>)">
+    <?= $this->renderSection('content')?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,34 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile</title>
-
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/style.css'); ?>">
-</head>
-<body>
+<?=$this->extend('layouts/app')?>
+<?=$this->section('content')?>
 <div class="content">
 <h1>Selamat Datang!</h1>
         <img src="<?php echo base_url('assets/img/passft.jpeg'); ?>" alt="photo profile"> <br>
-<table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><?=$nama?></td>
-</tr>
-<tr>
-            <td>Kelas</td>
-            <td>:</td>
-            <td><?=$kelas?></td>
-</tr>
-<tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><?=$npm?></td>
-</tr>
-</table>
+        <h1><?= $nama ?></h1>
+        <p>Kelas <?= $kelas ?></p>
+        <p>NPM <?= $npm ?></p>
 </div>
-</body>
-</html>
+<?=$this->endSection()?>
